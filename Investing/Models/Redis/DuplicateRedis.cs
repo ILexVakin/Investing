99efbbdd55000436fe22bbc0af2449fc08f6f4ec
Ilexvakin <ilexvakin@mail.ru>
2025-10-02 22:25:02 +0300

    Созданы 2 модели (оригинал и дубликат). Так как изображения повторяются, для тех которые повторятся, будут получены путем дублирования от оригинала из кэша.

---
+++ Investing/Models/Redis/DuplicateRedis.cs
@@ -0,0 +1,9 @@
+﻿namespace Investing.Models.Redis
+{
+    public class DuplicateRedis
+    {
+        public int Id_duplicate {  get; set; }
+        public int Original_id { get; set; }
+        public string Isin_duplicate { get; set; }
+    }
+}
---
+++ Investing/Models/Redis/OriginalRedis.cs
@@ -0,0 +1,8 @@
+﻿namespace Investing.Models.Redis
+{
+    public class OriginalRedis
+    {
+        public int Id_original { get; set; }
+        public string Isin_original { get; set; }
+    }
+}
